f64906c8fe507ef2ee671675bd307c7487700887
Mistral Contrastin <madgenhetic@gmail.com>
2012-08-08 19:35:47 +0300

    capfix

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "git@github.com:masgen/iron.git"
+set :repository, "git@github.com:Masgen/Iron.git"
 set :branch, "master"
 
 default_run_options[:pty] = true
