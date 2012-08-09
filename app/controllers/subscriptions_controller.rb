22798259ba9ca5f6e7cff5443d865b1ad9cdee6f
Mistral Contrastin <madgenhetic@gmail.com>
2012-08-09 14:02:44 +0300

    subscription authentication fix

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,6 +1,6 @@
 #encoding: utf-8
 class SubscriptionsController < ApplicationController
-  before_filter :authenticate
+  before_filter :authenticate, only: [:show]
   
   def show
     @subscriptions = Subscription.all
